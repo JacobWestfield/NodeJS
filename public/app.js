52398f0d436f0209154c5dd2aef9be96f0eb9545
JacobWestfield <zzultekk@gmail.com>
2023-05-20 22:38:02 +0300

    1

---
+++ public/app.js
@@ -0,0 +1,33 @@
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.type === "remove") {
+    const id = event.target.dataset.id;
+
+    remove(id).then(() => {
+      event.target.closest("li").remove();
+    });
+  }
+});
+
+async function remove(id) {
+  await fetch(`/${id}`, { method: "DELETE" });
+}
+
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.type === "edit") {
+    const id = event.target.dataset.id;
+    const newTitle = prompt("Enter new title");
+    if (!newTitle) return;
+    console.log(newTitle);
+    const a = document.getElementById(id);
+    edit(id, newTitle).then(() => {
+      a.innerHTML = newTitle;
+    });
+  }
+});
+
+async function edit(id, newTitle) {
+  await fetch(`/${id}/${newTitle}`, {
+    method: "PUT",
+    body: { title: JSON.stringify(newTitle) },
+  });
+}
